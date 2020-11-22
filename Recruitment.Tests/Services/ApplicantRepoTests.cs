437c9c6a4c0787dbe2e44eec1adf4b2965ec1059
munafs2301 <marvelousfrank5@gmail.com>
2020-11-22 16:13:34 +0100

    update

--- Recruitment.Tests/Services/ApplicantRepoTests.cs
+++ Recruitment.Tests/Services/ApplicantRepoTests.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Recruitment.Domain.Entities;
+using Recruitment.Domain.Enums;
+using Recruitment.Web.Models;
 using Recruitment.Web.Services;
 using System;
 using System.Collections.Generic;
@@ -11,10 +14,56 @@ namespace Recruitment.Web.Services.Tests
     [TestClass()]
     public class ApplicantRepoTests
     {
+
         [TestMethod()]
         public void CreateTest()
         {
-            Assert.Fail();
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            // Arrange
+            Applicant applicant = new Applicant() { ApplicantId = 1, FirstName = "Muna", LastName = "Frank", Age = 22, EmailAddress = "marvelousfrank5@gmail.com", Gender = Gender.Female };
+
+            //Act
+            ApplicantRepo arepo = new ApplicantRepo();
+            var result = arepo.Create(applicant).Result;
+
+            //Assert
+            Assert.IsTrue(result);
         }
+
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            // Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod()]
+        public void GetJobDetailsTest()
+        {
+            // Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            // Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            // Arrange
+            //Act
+            //Assert
+        }
+
+
+
     }
 }
